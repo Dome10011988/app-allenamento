efdd4466ac7f951ac64bcb7963c92f1db8588e0d
Dome10011988 <domenico.benegiamo@gmail.com>
2025-04-24 17:14:50 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installato");
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll(["/", "/index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
