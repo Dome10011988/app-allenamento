3e90a3fe1267650b19028631fdd7491ad9efc16a
Dome10011988 <domenico.benegiamo@gmail.com>
2025-04-24 18:31:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,48 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker installato");
-  e.waitUntil(
-    caches.open("static").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
-    })
+const CACHE_NAME = 'app-allenamento-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/programma-allenamento.html',
+  '/piano-alimentare.html',
+  '/lista-spesa-giornaliera.html',
+  '/aggiungi-peso.html',
+  '/icons/kettlebell-preview.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req)
+        .then(res => caches.open(CACHE_NAME)
+          .then(cache => { cache.put(req, res.clone()); return res; }))
+        .catch(() => caches.match('/index.html'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req)
+        .then(res => caches.open(CACHE_NAME)
+          .then(cache => { cache.put(req, res.clone()); return res; }))
+      )
+    );
+  }
 });
